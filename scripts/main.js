73f8738b3c05ead5efe4ba2bed589a0ab91f94e3
ahaoboy <504595380@qq.com>
2025-08-10 22:40:01 +0800

    init

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,22 @@
 var run = require("./run")
 
+
+
+var n = 100000
 var testLog = []
+
 function log(s) {
   testLog.push(s)
-  mp.osd_message(testLog.join("\n"), 100000);
+  mp.osd_message(testLog.join("\n"), n);
 }
-
-mp.set_property_native('pause', true);
-
 function startTest() {
-  a.v8v7(log)
+  run.v8v7(log)
+  testLog.push("Test has completed.")
+  mp.osd_message(testLog.join("\n"), n);
+
 }
+
+mp.osd_message("Test is starting...", n);
+mp.set_property_native('pause', true);
 
 setTimeout(startTest, 1000)
